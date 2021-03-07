060c03b7c16227a7f2d7ae911c7531b7707c00ed
Joshua <joycejosh7@gmail.com>
2021-03-07 16:03:57 -0500

    created function for handleSubmit

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ function init() {
     getAllActivities()
 }
 
+function handleSubmit(e) {
+    e.preventDefault()
+    const activityInfo = {
+        title: e.target[0].value,
+        creator: e.target[1].value,
+        description: e.target[2].value
+    }
+    // by default Fetch makes a GET request, if you want to make any other request you HAVE to tell it what kind
+    fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(activityInfo)
+    })
+        .then(r => r.json())
+        .then(data => putActivityOnDom(data))
+}
